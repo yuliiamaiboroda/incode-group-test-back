379ca218beec344bda8771236ec21f62e4d49d90
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2023-10-12 17:12:00 +0300

    users services refactored

--- helpers/users-query/index.js
+++ helpers/users-query/index.js
@@ -1,4 +1,14 @@
 const UserModel = require('../../models/');
+const { ROLES_LIST } = require('../../helpers/constants/user-roles');
+const { createHttpException } = require('../utils/create-http-exception');
+const {
+  UPDATE_DEFAULT_CONFIG,
+} = require('../../helpers/constants/update-default-config');
+const mongoose = require('mongoose');
+
+const getAdminUsers = async () => {
+  return await UserModel.find({}, { sessionKey: 0, passwordHash: 0 });
+};
 
 const getSubordinates = async bossId => {
   return await UserModel.find(
@@ -7,11 +17,118 @@ const getSubordinates = async bossId => {
   );
 };
 
+const getBossUsers = async user => {
+  const subordinates = await getSubordinates(user.id);
+
+  return {
+    _id: user._id,
+    name: user.name,
+    surname: user.surname,
+    role: user.role,
+    boss: user.boss,
+    email: user.email,
+    subordinates,
+  };
+};
+
 const getUserById = async id => {
-  return await UserModel.findById(id, { sessionKey: 0, passwordHash: 0 });
+  const user = await UserModel.findById(id, { sessionKey: 0, passwordHash: 0 });
+
+  if (!user) throw createHttpException(404, 'Not found');
+
+  return user;
+};
+
+const validateUserRole = async role => {
+  if (role === ROLES_LIST.USER) throw createHttpException(403, 'Forbidden');
+};
+
+const validateSelfBossChange = async (bossId, userId) => {
+  if (bossId === userId) throw createHttpException(403, 'Forbidden');
+};
+
+const validateBossIsSubordinate = async (subordinates, bossId) => {
+  const isBossSubordinate = subordinates.includes(bossId);
+  if (isBossSubordinate) throw createHttpException(403, 'Forbidden');
+};
+
+const validateSettingBossForAdmin = async role => {
+  if (role === ROLES_LIST.ADMIN) throw createHttpException(403, 'Forbidden');
+};
+
+const validateSubordateOfSuberbordinate = async (
+  newBossId,
+  usersSubordinates
+) => {
+  const bossesOfNewBossArray = await UserModel.find({
+    subordinates: newBossId,
+  });
+  const repeatedUsers = [];
+
+  bossesOfNewBossArray.forEach(({ boss }) => {
+    if (!boss) return;
+
+    if (usersSubordinates.includes(boss.toString())) {
+      repeatedUsers.push(boss);
+    }
+  });
+
+  if (repeatedUsers.length > 0)
+    throw createHttpException(
+      403,
+      "Forbidden. a subordinate of a subordinate user cannot be the user's boss"
+    );
+};
+
+const validateSameBoss = async (oldbossId, newBossId) => {
+  if (oldbossId.toString() === newBossId.toString())
+    throw createHttpException(
+      400,
+      'This user is already boss for current user'
+    );
+};
+
+const updateOldBoss = async (oldBossId, oldBossRole, userId) => {
+  return await UserModel.findByIdAndUpdate(
+    oldBossId,
+    {
+      role: oldBossRole,
+      $pull: { subordinates: new mongoose.Types.ObjectId(userId) },
+    },
+    { ...UPDATE_DEFAULT_CONFIG, projection: { passwordHash: 0, sessionKey: 0 } }
+  );
+};
+
+const updateNewBoss = async (newBossId, newBossRole, userId) => {
+  return await UserModel.findByIdAndUpdate(
+    newBossId,
+    {
+      role: newBossRole,
+      $push: { subordinates: new mongoose.Types.ObjectId(userId) },
+    },
+    { ...UPDATE_DEFAULT_CONFIG, projection: { passwordHash: 0, sessionKey: 0 } }
+  );
+};
+
+const updateUser = async (userId, newBossId) => {
+  return await UserModel.findByIdAndUpdate(
+    userId,
+    { boss: new mongoose.Types.ObjectId(newBossId) },
+    { ...UPDATE_DEFAULT_CONFIG, projection: { passwordHash: 0, sessionKey: 0 } }
+  );
 };
 
 module.exports = {
-  getSubordinates,
+  getAdminUsers,
+  getBossUsers,
   getUserById,
+  validateUserRole,
+  validateSelfBossChange,
+  validateBossIsSubordinate,
+  validateSettingBossForAdmin,
+  validateSubordateOfSuberbordinate,
+  validateSameBoss,
+  updateOldBoss,
+  updateNewBoss,
+  updateUser,
 };
--- services/users/index.js
+++ services/users/index.js
@@ -1,35 +1,32 @@
-const UserModel = require('../../models/');
 const { ROLES_LIST } = require('../../helpers/constants/user-roles');
 const {
   createHttpException,
 } = require('../../helpers/utils/create-http-exception');
-const { getSubordinates, getUserById } = require('../../helpers/users-query');
 const {
-  UPDATE_DEFAULT_CONFIG,
-} = require('../../helpers/constants/update-default-config');
+  getAdminUsers,
+  getBossUsers,
+  getUserById,
+  validateUserRole,
+  validateSelfBossChange,
+  validateBossIsSubordinate,
+  validateSettingBossForAdmin,
+  validateSubordateOfSuberbordinate,
+  validateSameBoss,
+  updateOldBoss,
+  updateNewBoss,
+  updateUser,
+} = require('../../helpers/users-query');
 
 const getUsersList = async user => {
-  const { role, id } = user;
-
-  switch (role) {
+  switch (user.role) {
     case ROLES_LIST.ADMIN:
-      return await UserModel.find({}, { sessionKey: 0, passwordHash: 0 });
+      return await getAdminUsers();
 
     case ROLES_LIST.BOSS:
-      const subordinates = await getSubordinates(id);
-      const bossInfo = {
-        _id: user._id,
-        name: user.name,
-        surname: user.surname,
-        role: user.role,
-        boss: user.boss,
-        email: user.email,
-        subordinates: subordinates,
-      };
-      return bossInfo;
+      return getBossUsers(user);
 
     case ROLES_LIST.USER:
-      return await getUserById(id);
+      return await getUserById(user.id);
 
     default:
       return [];
@@ -39,90 +36,72 @@ const getUsersList = async user => {
 const changeBossOfUser = async (currentUserData, body) => {
   const { id, subordinates, role } = currentUserData;
   const { bossId, userId } = body;
+  try {
+    // Forbid change user`s boss for user with role User
+    await validateUserRole(role);
 
-  // Forbid change user`s boss for user with role User
-  if (role === ROLES_LIST.USER) throw createHttpException(403, 'Forbidden');
+    // Forbid set as a boss for yourself
+    await validateSelfBossChange(bossId, userId);
 
-  // Forbid set as a boss for yourself
-  if (bossId === userId) throw createHttpException(403, 'Forbidden');
+    const user = await getUserById(userId);
 
-  const user = await getUserById(userId);
+    // Forbid set subordinates as a boss
+    await validateBossIsSubordinate(user.subordinates, bossId);
 
-  if (!user) throw createHttpException(404, 'Not found');
+    // Forbid set boss for users that have ADMIN role
+    await validateSettingBossForAdmin(user.role);
 
-  // Forbid set subordinates as a boss
-  const isBossSubordinate = user.subordinates.includes(bossId);
-  if (isBossSubordinate) throw createHttpException(403, 'Forbidden');
+    const isUserSubordinate = subordinates.includes(userId);
+    const isCurrenUserBoss = id === user.boss.toString();
 
-  // Forbid set boss for users that have ADMIN role
-  if (user.role === ROLES_LIST.ADMIN)
-    throw createHttpException(403, 'Forbidden');
+    // Forbid change boss in cases when user is not your subordinate or you dont have ADMIN role
+    if (
+      (!isUserSubordinate || !isCurrenUserBoss) &&
+      role !== ROLES_LIST.ADMIN
+    ) {
+      throw createHttpException(403, 'Forbidden');
+    }
 
-  const isUserSubordinate = subordinates.includes(userId);
-  const isCurrenUserBoss = id === user.boss.toString();
+    let newBossRole;
+    let oldBossRole;
 
-  // Forbid change boss in cases when user is not your subordinate or you dont have ADMIN role
-  if ((!isUserSubordinate || !isCurrenUserBoss) && role !== ROLES_LIST.ADMIN) {
-    throw createHttpException(403, 'Forbidden');
+    const newBoss = await getUserById(bossId);
+
+    if (!newBoss) throw createHttpException(404, 'Not found');
+
+    // Forbit set as a boss user who is subordinate to your subordinate
+
+    await validateSubordateOfSuberbordinate(newBoss.id, subordinates);
+
+    newBoss.role === ROLES_LIST.ADMIN
+      ? (newBossRole = ROLES_LIST.ADMIN)
+      : (newBossRole = ROLES_LIST.BOSS);
+
+    const oldBoss = await getUserById(user.boss);
+
+    // Forbid change boss on the same user
+    await validateSameBoss(oldBoss.id, newBoss.id);
+
+    oldBoss.role === ROLES_LIST.ADMIN && (oldBossRole = ROLES_LIST.ADMIN);
+
+    oldBoss.subordinates.length === 1 && oldBoss.role !== ROLES_LIST.ADMIN
+      ? (oldBossRole = ROLES_LIST.USER)
+      : (oldBossRole = oldBoss.role);
+
+    const updatedOldBoss = await updateOldBoss(user.boss, oldBossRole, user.id);
+
+    const updatedNewBoss = await updateNewBoss(bossId, newBossRole, user.id);
+
+    const updatedUser = await updateUser(userId, newBoss.id);
+
+    return {
+      updatedOldBoss,
+      updatedNewBoss,
+      updatedUser,
+    };
+  } catch (err) {
+    throw err;
   }
-
-  let newBossRole;
-  let oldBossRole;
-
-  const newBoss = await getUserById(bossId);
-
-  if (!newBoss) throw createHttpException(404, 'Not found');
-
-  newBoss.role === ROLES_LIST.ADMIN
-    ? (newBossRole = ROLES_LIST.ADMIN)
-    : (newBossRole = ROLES_LIST.BOSS);
-
-  const oldBoss = await getUserById(user.boss);
-
-  if (!oldBoss) throw createHttpException(404, 'Not found');
-
-  // Forbid change boss on the same user
-  if (oldBoss.id.toString() === newBoss.id.toString())
-    throw createHttpException(
-      400,
-      'This user is already boss for current user'
-    );
-
-  oldBoss.role === ROLES_LIST.ADMIN && (oldBossRole = ROLES_LIST.ADMIN);
-
-  oldBoss.subordinates.length === 1 && oldBoss.role !== ROLES_LIST.ADMIN
-    ? (oldBossRole = ROLES_LIST.USER)
-    : (oldBossRole = oldBoss.role);
-
-  const updatedOldBoss = await UserModel.findByIdAndUpdate(
-    user.boss,
-    {
-      role: oldBossRole,
-      $pull: { subordinates: user.id },
-    },
-    { ...UPDATE_DEFAULT_CONFIG, projection: { passwordHash: 0, sessionKey: 0 } }
-  );
-
-  const updatedNewBoss = await UserModel.findByIdAndUpdate(
-    bossId,
-    {
-      role: newBossRole,
-      $push: { subordinates: user.id },
-    },
-    { ...UPDATE_DEFAULT_CONFIG, projection: { passwordHash: 0, sessionKey: 0 } }
-  );
-
-  const updatedUser = await UserModel.findByIdAndUpdate(
-    userId,
-    { boss: newBoss.id },
-    { ...UPDATE_DEFAULT_CONFIG, projection: { passwordHash: 0, sessionKey: 0 } }
-  );
-
-  return {
-    updatedOldBoss,
-    updatedNewBoss,
-    updatedUser,
-  };
 };
 
 module.exports = {
